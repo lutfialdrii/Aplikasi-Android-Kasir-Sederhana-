91fbe5490a2e42bb543a2aabdaef0cf3bc8a08d7
Lutfi Aldri Permana <83573252+lutfialdrii@users.noreply.github.com>
2022-02-28 11:33:37 +0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.aplikasikasir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView tvJumlahBayar, tvHarga,tvBarang, tvTotal, tvDiskon, tvKodeTransaksi;
+    private EditText etKodeBarang, etJumlah, etKodeDiskon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout2);
+
+        etKodeDiskon = findViewById(R.id.etKodeDiskon);
+        etKodeBarang = findViewById(R.id.etKodeBarang);
+        etJumlah = findViewById(R.id.etJumlah);
+        Button btnHitung = findViewById(R.id.btnHitung);
+        tvJumlahBayar = findViewById(R.id.tvJumlahBayar);
+        tvBarang = findViewById(R.id.tvBarang);
+        tvTotal = findViewById(R.id.tvTotal);
+        tvDiskon = findViewById(R.id.tvDiskon);
+        tvHarga = findViewById(R.id.tvHarga);
+        tvKodeTransaksi = findViewById(R.id.tvKodeTransaksi);
+
+        btnHitung.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.btnHitung) {
+            //proses menghitung
+            String Kode = etKodeBarang.getText().toString().trim();
+            String Jumlah = etJumlah.getText().toString().trim();
+            String KodeDiskon = etKodeDiskon.getText().toString().trim();
+
+            //Cek
+            boolean tidakdiinput = false;
+            if (TextUtils.isEmpty(Kode)) {
+                tidakdiinput = true;
+                etKodeBarang.setError("Tidak boleh kosong!");
+            }
+            if (TextUtils.isEmpty(Jumlah)) {
+                tidakdiinput = true;
+                etJumlah.setError("Tidak boleh kosong!");
+            }
+            if (TextUtils.isEmpty(KodeDiskon)) {
+                tidakdiinput = true;
+                etKodeDiskon.setError("Tidak boleh kosong!");
+            }
+            if (!tidakdiinput) {
+                if (etKodeBarang.getText().toString().equals("AND")) {
+                    String nama1 = "Android";
+                    int harga1 = 1000000;
+                    tvBarang.setText(nama1);
+                    tvHarga.setText(String.valueOf(harga1));
+
+                    String kodetransaksi = etKodeBarang.getText().toString() + etKodeDiskon.getText().toString();
+                    tvKodeTransaksi.setText(kodetransaksi);
+
+                    int totalHarga = harga1 * Integer.parseInt( etJumlah.getText().toString());
+                    tvTotal.setText("" + totalHarga);
+
+                    int Diskon = (totalHarga * Integer.parseInt(etKodeDiskon.getText().toString() ))/ 100;
+                    tvDiskon.setText(""+ Diskon);
+
+                    int JumlahBayar = totalHarga - Diskon;
+                    tvJumlahBayar.setText("" + JumlahBayar);
+                }
+            }
+        }
+    }
+}
